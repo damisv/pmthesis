25279b5d2d8c0f8f999d7d2218168b6d7034e1c9
Hibye-Yo <mpampik@windowslive.com>
2017-05-23 19:18:46 +0300

    Last commit fix

--- assets/app/webapp/content/dashboard/dashboard.component.ts
+++ assets/app/webapp/content/dashboard/dashboard.component.ts
@@ -3,6 +3,11 @@ import {Title} from "@angular/platform-browser";
 import {ProfileService} from "../../_services/profile.service";
 import {Profile} from "../../../models/profile";
 import {Subscription} from "rxjs/Subscription";
+import {TaskService} from "../../_services/task.service";
+import {Router} from "@angular/router";
+import {ProjectService} from "../../_services/projects.service";
+import {Project} from "../../../models/project";
+import {Task} from "../../../models/task";
 
 @Component({
     selector: 'webapp-dashboard',
@@ -19,15 +24,46 @@ export class DashboardComponent implements OnInit{
         }
     );
 
-    constructor(private profileService: ProfileService,private titleService: Title){
+    projects:Project[];
+    projectSubscription:Subscription = this.projectService.projects$.subscribe(
+        projects =>{
+            this.projects = projects;
+        }
+    );
+
+    tasks:Task[];
+
+    constructor(private profileService: ProfileService,
+                private titleService: Title,
+                private projectService:ProjectService,
+                private router:Router,
+                private taskService:TaskService){
     }
 
     ngOnInit(){
         this.titleService.setTitle("My Dashboard");
+        this.taskService.get().subscribe(
+            res => {
+                this.tasks = res.tasks;
+            }
+        );
     }
+
+    openProjectDashboard(index){
+        this.projectService.giveProject(this.projects[index]);
+        this.router.navigate(['app','project','dashboard']);
+    }
+
+    openTaskView(index){
+        this.taskService.giveTask(this.tasks[index]);
+        this.router.navigate(['app','project','taskview']);
+    }
+
     ngOnDestroy(){
         if(this.subscription!==undefined)
-        this.subscription.unsubscribe();
+            this.subscription.unsubscribe();
+        if(this.projectSubscription!==undefined)
+            this.projectSubscription.unsubscribe();
         this.titleService.setTitle("Project Management");
     }
 }
